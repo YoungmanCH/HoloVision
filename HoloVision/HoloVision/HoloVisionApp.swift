992c941e43b1e5cf30558f441c7a0dcc7ee08c0d
YoungmanCH <tsubasa22business@gmail.com>
2024-06-18 18:05:54 +0900

    Sample red ball

---
+++ HoloVision/HoloVision/HoloVisionApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoloVisionApp.swift
+//  HoloVision
+//
+//  Created by Youngman on 2024/06/18.
+//
+
+import SwiftUI
+
+@main
+struct HoloVisionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
