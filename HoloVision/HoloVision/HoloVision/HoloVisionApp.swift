8eb68aefe0e9084198c5ede40359adc2f83f1cd7
YoungmanCH <tsubasa22business@gmail.com>
2024-06-20 11:14:51 +0900

    Finish: sample

--- HoloVision/HoloVision/HoloVision/HoloVisionApp.swift
+++ HoloVision/HoloVision/HoloVision/HoloVisionApp.swift
@@ -7,16 +7,23 @@
 
 import SwiftUI
 
+@Observable
+class RifleData {
+    var rolledNumber = 0
+}
+
 @main
 struct HoloVisionApp: App {
+    @State var rifleData = RifleData()
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView(rifleData: rifleData)
         }
         .defaultSize(width: 100, height: 100)
 
         ImmersiveSpace(id: "ImmersiveSpace") {
-            ImmersiveView()
+            ImmersiveView(rifleData: rifleData)
         }
     }
 }
--- HoloVision/HoloVision/HoloVision/ImmersiveView.swift
+++ HoloVision/HoloVision/HoloVision/ImmersiveView.swift
@@ -9,7 +9,17 @@ import SwiftUI
 import RealityKit
 import RealityKitContent
 
+let rifleMap = [
+//  + | -
+    [1, 6],
+    [4, 3],
+    [2, 5],
+]
+
 struct ImmersiveView: View {
+    var rifleData: RifleData
+    @State var droppedRifle = false;
+    
     var body: some View {
         RealityView { content in
             let floor = ModelEntity(mesh: .generatePlane(width: 50, depth: 50), materials: [OcclusionMaterial()])
@@ -22,21 +32,24 @@ struct ImmersiveView: View {
             content.add(floor)
             
             if let rifleModel = try? await Entity(named: "M91_rifle"),
-               let rifle = rifleModel.children.first?.children.first {
+               let rifle = rifleModel.children.first?.children.first,
+               let environment = try? await EnvironmentResource(named: "whiteStudio")
+            {
                 rifle.scale = [0.01, 0.01, 0.01]
                 rifle.position.y = 0.5
                 rifle.position.z = -1
                 
                 rifle.generateCollisionShapes(recursive: true)
                 let collisionShape = ShapeResource.generateSphere(radius: 0.1)
-                
-                // 落下させるなら、generateBoxを使うと良い。なんかよくわかりません。かなり調べる必要があると思う。
-//                let collisionShapeOfSize = ShapeResource.generateBox(size: [0.01, 0.11, 0.01])])
-                
+
                 rifle.components.set([
                     CollisionComponent(shapes: [collisionShape]),
                     InputTargetComponent()
                 ])
+                
+                rifle.components.set(ImageBasedLightComponent(source: .single(environment)))
+                rifle.components.set(ImageBasedLightReceiverComponent(imageBasedLight: rifle))
+                rifle.components.set(GroundingShadowComponent(castsShadow: true))
                 
                 rifle.components[PhysicsBodyComponent.self] = .init(PhysicsBodyComponent(
                     massProperties: .default,
@@ -47,6 +60,26 @@ struct ImmersiveView: View {
                 rifle.components[PhysicsMotionComponent.self] = .init()
                                 
                 content.add(rifle)
+                
+                let _ = content.subscribe(to: SceneEvents.Update.self) { event in
+                    guard droppedRifle else { return }
+                    guard let rifleMotion = rifle.components[PhysicsMotionComponent.self] else { return }
+                    
+                    if simd_length(rifleMotion.linearVelocity) < 0.1 && simd_length(rifleMotion.angularVelocity) < 0.1 {
+                        let xDirection = rifle.convert(direction: SIMD3(x: 1, y: 0, z: 0), to: nil)
+                        let yDirection = rifle.convert(direction: SIMD3(x: 0, y: 1, z: 0), to: nil)
+                        let zDirection = rifle.convert(direction: SIMD3(x: 0, y: 0, z: 1), to: nil)
+                        
+                        let greatestDirection = [
+                            0: xDirection.y,
+                            1: yDirection.y,
+                            2: zDirection.y,
+                        ]
+                            .sorted(by: { abs($0.1) > abs($1.1) })[0]
+                        
+                        rifleData.rolledNumber = rifleMap[greatestDirection.key][greatestDirection.value > 0 ? 0 : 1]
+                    }
+                }
             }
         }
         .gesture(dragGesture)
@@ -61,10 +94,19 @@ struct ImmersiveView: View {
             }
             .onEnded { value in
                 value.entity.components[PhysicsBodyComponent.self]?.mode = .dynamic
+                
+                if !droppedRifle {
+                    Timer.scheduledTimer(withTimeInterval: 1, repeats: false) { _ in droppedRifle = true
+                    }
+                }
             }
     }
 }
 
 #Preview(immersionStyle: .mixed) {
-    ImmersiveView()
+    ImmersiveView(rifleData: RifleData())
 }
+
+
+// 落下させるなら、generateBoxを使うと良い。なんかよくわかりません。かなり調べる必要があると思う。
+//                let collisionShapeOfSize = ShapeResource.generateBox(size: [0.01, 0.11, 0.01])])
